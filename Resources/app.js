3c756dc9949e2c8eb00b4171b4e92c93661cb3a2
mide765 <mide.smile@gmail.com>
2014-03-08 00:00:15 +0100

    First working RSS feed
    Finally got it to work and it looks something like this
    http://i.imgur.com/LKZRJGB.png . I got the help for the code from this
    YouTube video http://www.youtube.com/watch?v=_6g-ela2S58 .

--- Resources/app.js
+++ Resources/app.js
@@ -1,7 +1,44 @@
-Ti.UI.setBackgroundColor('#F8F8F8');
-var win1 = require('getXML');
-var XMLWin = win1({
-	title: 'XML',
-	weekNo:'12'
+var win = Ti.UI.createWindow();
+ 
+var view = Ti.UI.createView();
+ 
+var table = Ti.UI.createTableView();
+ 
+view.add(table);
+win.add(view);
+
+var url = "http://realitatea.feedsportal.com/c/32533/fe.ed/rss.realitatea.net/stiri.xml", data = [];
+ 
+//create the xhr
+var xhr = Ti.Network.createHTTPClient({
+  onload : function(e) {
+ 
+    //get the returned xml
+		var doc = this.responseXML.documentElement;
+    //get an array of all items from feed
+		var items = doc.getElementsByTagName("item");
+		
+//traverse through items
+		for(var c = 0; c < items.length; c++){
+      //get current item
+			var item = items.item(c);
+ 
+      //get title of the item
+			var title = item.getElementsByTagName("title").item(0).text;
+ 
+      //push a new row to the data array
+			data.push({title:title});
+		}
+		
+		table.data = data;
+	},
+	onerror : function(e) {
+		alert("error");
+	},
+	timeout : 5000
 });
-XMLWin.open();
+ 
+xhr.open('GET', url);
+xhr.send();
+ 
+win.open();
