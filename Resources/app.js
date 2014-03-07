83acf94d4f892f8fee6e42abd4d4f2878b768dd3
mide765 <mide.smile@gmail.com>
2014-03-07 15:09:08 +0100

    Created a Window and added two Labels to it.
    There can only be one Window showing at a time, but there can be
    multiple labels on the same page (Image, Table, Scroll).

--- Resources/app.js
+++ Resources/app.js
@@ -1,64 +1,25 @@
-// this sets the background color of the master UIView (when there are no windows/tab groups on it)
-Titanium.UI.setBackgroundColor('#000');
+//Create Window.
+var homePage = Titanium.UI.createWindow({
+	backgroundColor: 'white'
+	});
 
-// create tab group
-var tabGroup = Titanium.UI.createTabGroup();
-
-
-//
-// create base UI tab and root window
-//
-var win1 = Titanium.UI.createWindow({  
-    title:'Tab 1',
-    backgroundColor:'#fff'
+//Create View
+var actor = Ti.UI.createView({
+	backgroundColor: 'red',
+	width: 500,
+	height: 500,
+	top: 20
 });
-var tab1 = Titanium.UI.createTab({  
-    icon:'KS_nav_views.png',
-    title:'Tab 1',
-    window:win1
-});
-
-var label1 = Titanium.UI.createLabel({
-	color:'#999',
-	text:'I am Window 1',
-	font:{fontSize:20,fontFamily:'Helvetica Neue'},
-	textAlign:'center',
-	width:'auto'
+var actor1 = Ti.UI.createView({
+	backgroundColor: 'blue',
+	width: 500,
+	height: 500,
+	top: 540
 });
 
-win1.add(label1);
+//Add the View to the Window
+homePage.add(actor);
+homePage.add(actor1);
 
-//
-// create controls tab and root window
-//
-var win2 = Titanium.UI.createWindow({  
-    title:'Tab 2',
-    backgroundColor:'#fff'
-});
-var tab2 = Titanium.UI.createTab({  
-    icon:'KS_nav_ui.png',
-    title:'Tab 2',
-    window:win2
-});
-
-var label2 = Titanium.UI.createLabel({
-	color:'#999',
-	text:'I am Window 2',
-	font:{fontSize:20,fontFamily:'Helvetica Neue'},
-	textAlign:'center',
-	width:'auto'
-});
-
-win2.add(label2);
-
-
-
-//
-//  add tabs
-//
-tabGroup.addTab(tab1);  
-tabGroup.addTab(tab2);  
-
-
-// open tab group
-tabGroup.open();
+//Open Window
+homePage.open();
